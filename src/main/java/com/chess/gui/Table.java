66cf49d8f22957909a16db20ceca02c2313000e6
Arantog <arantog@gmail.com>
2021-06-04 17:30:36 +0200

    adding menu bar

--- src/main/java/com/chess/gui/Table.java
+++ src/main/java/com/chess/gui/Table.java
@@ -1,15 +1,41 @@
 package com.chess.gui;
 
+import javax.print.DocFlavor;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Table {
     private final JFrame gameFrame;
-    private static Dimension WINDOW_DIMENSION = new Dimension(1080,720);
+    private static Dimension WINDOW_DIMENSION = new Dimension(1080, 720);
 
     public Table() {
         this.gameFrame = new JFrame("Chess");
+        final JMenuBar tableMenuBar = new JMenuBar();
+        populateMenuBar(tableMenuBar);
+        this.gameFrame.setJMenuBar(tableMenuBar);
         this.gameFrame.setVisible(true);
         this.gameFrame.setSize(WINDOW_DIMENSION);
+
     }
+
+    private void populateMenuBar(final JMenuBar tableMenuBar) {
+        tableMenuBar.add(createFileMenu());
+    }
+
+    private JMenu createFileMenu() {
+        final JMenu fileMenu = new JMenu("File");
+
+        final JMenuItem openPGN = new JMenuItem("Load from PGN file");
+        openPGN.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("opening PGN file");                     //TODO
+            }
+        });
+        fileMenu.add(openPGN);
+        return fileMenu;
+    }
+
 }
